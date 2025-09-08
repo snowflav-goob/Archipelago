6ec4f3d352f335f5c6d116c12b243734cdb1ba80
James White <james_white_d@hotmail.com>
2025-09-08 16:40:55 +0100

    Bump version to 5.1.0-alpha.3

--- worlds/pokemon_crystal/data.py
+++ worlds/pokemon_crystal/data.py
@@ -9,7 +9,7 @@ import yaml
 
 from BaseClasses import ItemClassification
 
-APWORLD_VERSION = "5.1.0-alpha.2"
+APWORLD_VERSION = "5.1.0-alpha.3"
 POKEDEX_OFFSET = 10000
 POKEDEX_COUNT_OFFSET = 20000
 FLY_UNLOCK_OFFSET = 512
