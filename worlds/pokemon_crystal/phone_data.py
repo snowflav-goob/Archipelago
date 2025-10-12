3d7c7c810c4b12bc0c48cc13695c6ea2334b7f55
Darvitz <vgm.shinshiba@gmail.com>
2025-10-12 14:25:01 -0400

    Pokecrystal hint regions (#337)
    * Update phone_data.py
    
    first attempt at Pokemon Regions in Phone hints
    
    * Update phone_data.py
    
    * Update phone_data.py
    
    * Update phone_data.py
    
    * Update phone_data.py
    
    Adding Pokemon Mystery Dungeon Explorers of Sky using the region name 'Grass Continent'
    
    * Update worlds/pokemon_crystal/phone_data.py
    
    Co-authored-by: Snowflav_ <snowflav@protonmail.com>
    
    ---------
    
    Co-authored-by: Snowflav_ <snowflav@protonmail.com>

--- worlds/pokemon_crystal/phone_data.py
+++ worlds/pokemon_crystal/phone_data.py
@@ -166,7 +166,25 @@ def template_call_remote(location: Location, world):
     # split into lines with cont
     location_cmd = split_location(location.name.upper())
 
-    game_name = location.item.game.upper()
+    POKEMON_REGIONS = {
+        "POKEMON RED AND BLUE": "KANTO",
+        "POKEMON CRYSTAL": "JOHTO",
+        "POKEMON EMERALD": "HOENN",
+        "POKEMON FIRERED AND LEAFGREEN": "KANTO",
+        "POKEMON PLATINUM": "SINNOH",
+        "POKEMON BLACK AND WHITE": "UNOVA",
+        "VOLTORB FLIP": "the GAME CORNER",
+        "POKEMON PINBALL": "KANTO GAME CORNER",
+        "POKEMON MYSTERY DUNGEON EXPLORERS OF SKY": "GRASS CONTINENT"
+    }
+
+    raw_game_name = location.item.game.upper()
+
+    if raw_game_name in POKEMON_REGIONS:
+        game_name = POKEMON_REGIONS[raw_game_name]
+    else:
+        game_name = raw_game_name
+        
     game_name = (game_name[:15] + "…") if len(game_name) > 16 else game_name
 
     player_name = world.multiworld.player_name[player].upper()
